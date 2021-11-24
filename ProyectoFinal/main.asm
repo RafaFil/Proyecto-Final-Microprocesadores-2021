cbba5b6c434b92fc2e6bc32490157a4fa20c8ee2
unknown <filardisafa@gmail.com>
2021-11-23 21:05:49 -0300

    Rafa

--- ProyectoFinal/main.asm
+++ ProyectoFinal/main.asm
@@ -14,23 +14,6 @@
 	nums: .byte 512		; defino espacio en memoria para almacenar 512 números de un byte
 
 .CSEG
-setupADC:
-	; ADCSRA |= (1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-	ldi		r16,	0b11101111
-	sts		ADCSRA,	r16
-	
-	; ADMUX |= (1<<REFS0);
-	ldi		r16,	0b01000000
-	sts		ADMUX,	r16
-
-	ldi		r31,	0x00	; uso como bandera para leer una sola seed para los números random
-
-	sei
-
-setupPuntero:
-	ldi		r26,	low(nums)		; configuro registro X para apuntar a la primera dirección del
-	ldi		r27,	high(nums)		; espacio de memoria de los números
-
 convADC:
 	in		r30,	SREG	; guardo contexto
 
@@ -42,34 +25,47 @@ convADC:
 	out		SREG,	r30		; recupero el contexto
 	reti
 
+setupADC:
+	; ADCSRA |= (1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+	ldi		r16,	0b11101111
+	sts		ADCSRA,	r16
+	
+	; ADMUX |= (1<<REFS0);
+	ldi		r16,	0b01000000
+	sts		ADMUX,	r16
+
+	ldi		r31,	0x00	; uso como bandera para leer una sola seed para los números random
+	ldi		r20,	0x00	; valor para comparar cuándo salgo del bucle de generar números
+
+	sei
+
+setupPuntero:
+	ldi		r26,	low(nums)		; configuro registro X para apuntar a la primera dirección del
+	ldi		r27,	high(nums)		; espacio de memoria de los números
+
 start:
     cpi		r31,	0x00	; mientras no haya leído la seed, espero
 	breq	start
 
-	ldi		r18,	low(512)	; cargo en este par de registros la cantidad de números a generar
-	ldi		r19,	high(512)
+	ldi		r18,	low(511)	; cargo en este par de registros la cantidad de números a generar
+	ldi		r19,	high(511)
+
+bucleGenerador:
+	call	generaNumero
+	dec		r18
+	cpse	r18,	r20
+	rjmp	bucleGenerador
+
+	dec		r19
+	cpse	r19,	r20
+	rjmp	bucleGenerador
+
+	rjmp	PC+0
 
 generaNumero:
-	dec		r18				; resto 1 a los números que me quedan generar
-	cpi		r18,	0xFF	; si el r18 quedó en 0xFF, es porque "me llevé" una unidad del r19
-	brne	1				
-	dec		r19				; entonces le resto una unidad a r19
-
-	cpi		r19,	0x00	; si r19 es 0, ya generé todos los números necesarios, me voy
-	; irse
-
-	; genero un número de alguna forma medio mágica en el r20
-
-	st		X+,		r20		; guardo el número generado en la dirección del puntero X e incremento el puntero
-
-	rjmp	generaNumero	; repito
-
-modulo9:
-	subi	r21,	9		; resto 9 a r21
-	brlt	1				; si r21 era menor que 9 antes de la resta, dejo de restarle
-	rjmp	modulo9
-
-	add		r21,	9		; y le vuelvo a sumar 9, para quedar con un número entre 0 y 8
+	add		r16,	r16		; lo agrego con sí mismo y guardo el módulo 255
+	st		X+,		r16		; guardo el número generado en la dirección del puntero X e incremento el puntero
+	ret
 
 fin:
 	rjmp	fin
