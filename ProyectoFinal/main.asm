3a2f92d4371ddd76bc55546f0a929ae5eaf1993c
RafaFil <filardisafa@gmail.com>
2021-11-29 23:44:40 -0300

    Intento de hacer andar el USART

--- ProyectoFinal/main.asm
+++ ProyectoFinal/main.asm
@@ -6,7 +6,7 @@
 ;
 
 .ORG	0x0000
-	jmp		setupADC
+	jmp		setupUsart
 .ORG	0x002A
 	jmp		convADC
 
@@ -26,6 +26,16 @@ convADC:
 
 	out		SREG,	r30		; recupero el contexto
 	reti
+
+setupUsart:
+	ldi		r16,	103	; establece el baudrate of the transmission
+	out		UBRR0L, r16
+	sbi		UCSR0B,	4; habilita la recepcion (rxen0)
+	sbi		UCSR0B,	3; habilita la transmission (txen0)
+	sbi		UCSR0C, 2; seteo frame de 8 bits, los bits UCSZ01 y UCSZ00
+	sbi		UCSR0C, 1
+
+
 
 setupADC:
 	; ADCSRA |= (1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
